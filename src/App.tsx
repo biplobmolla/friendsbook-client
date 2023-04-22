25f07e282c6b903aae6785c59f2b4f72fc1566ad
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-04-22 20:40:57 +0600

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,40 @@
 import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { BiUserCircle, BiMessageRounded } from "react-icons/bi";
+import { IoMdNotifications } from "react-icons/io";
 
 function Header() {
   return (
     <>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">friendsbook</Link>
+      <div className="px-10 py-4 bg-slate-300 z-50 fixed w-screen top-0 left-0">
+        <ul className="flex justify-between items-center">
+          <li className="basis-1/5">
+            <Link to="/" className="text-xl font-medium">
+              friendsbook
+            </Link>
+          </li>
+          <li className="w-2/5 bg-white flex items-center relative py-2.5 rounded-full overflow-hidden">
+            <input
+              type="search"
+              placeholder="Search..."
+              className="absolute w-full h-full top-0 left-0 outline-none px-4"
+            />
+            <div>
+              <BsSearch />
+            </div>
+          </li>
+          <li className="basis-1/5">
+            <ul className="flex justify-end items-center gap-x-4 text-3xl">
+              <li>
+                <BiMessageRounded className="cursor-pointer" />
+              </li>
+              <li>
+                <BiUserCircle className="cursor-pointer" />
+              </li>
+              <li>
+                <IoMdNotifications className="cursor-pointer" />
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
